cdf13dcb9d79e1224f8a24f2682d2f7d3afff8d9
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-05-13 11:29:50 +0700

    chore: add no-console to eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ const config = {
     'prettier',
   ],
   rules: {
+    'no-console': 'warn',
     '@typescript-eslint/triple-slash-reference': 'off',
     '@typescript-eslint/naming-convention': [
       'warn',
