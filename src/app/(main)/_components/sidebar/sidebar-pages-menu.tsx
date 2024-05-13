9c0d3dbdbaf33cec177a68c162b8b2ae4a483133
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-05-13 11:31:47 +0700

    feat: remove settings menu button from sidebar

--- src/app/(main)/_components/sidebar/sidebar-pages-menu.tsx
+++ src/app/(main)/_components/sidebar/sidebar-pages-menu.tsx
@@ -1,9 +1,4 @@
-import {
-  MessageSquareMoreIcon,
-  UsersIcon,
-  BellIcon,
-  SettingsIcon,
-} from 'lucide-react';
+import { MessageSquareMoreIcon, UsersIcon, BellIcon } from 'lucide-react';
 import { Button } from '~/components/ui/button';
 
 export type SidebarPagesButtonProps = {
@@ -14,19 +9,18 @@ export const carouselPagesMenuItem = [
   { label: 'chats', icon: <MessageSquareMoreIcon size={16} />, pageIdx: 0 },
   { label: 'friends', icon: <UsersIcon size={16} />, pageIdx: 1 },
   { label: 'notifications', icon: <BellIcon size={16} />, pageIdx: 2 },
-  { label: 'settings', icon: <SettingsIcon size={16} />, pageIdx: 3 },
 ];
 
 export function SidebarPagesMenu(props: SidebarPagesButtonProps) {
   const { onPageChange } = props;
 
   return (
-    <div className='flex w-full border-b border-slate-200 px-2 pb-2'>
+    <div className='flex w-full border-b border-slate-200 p-2'>
       {carouselPagesMenuItem.map(item => (
         <Button
           key={item.pageIdx}
           variant='ghost'
-          className='h-auto w-1/4 flex-col gap-1 text-xs'
+          className='h-auto w-1/3 flex-col gap-1 text-xs'
           onClick={() => {
             onPageChange(item.pageIdx);
           }}
